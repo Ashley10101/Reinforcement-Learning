7010d45015be8edc0c9940f9a6527d11ff94bc1f
Ashely <Yiyun.L.Evelyn@gmail.com>
2023-10-30 17:18:38 +0800

    submit

---
+++ Q-learning/Maze/main.py
@@ -0,0 +1,17 @@
+"""
+Reinforcement learning maze example.
+
+Red rectangle:          explorer.
+Black rectangles:       hells       [reward = -1].
+Yellow bin circle:      paradise    [reward = +1].
+All other states:       ground      [reward = 0].
+
+"""
+
+import numpy as np
+import time
+import sys
+if sys.version_info.major == 2:
+    import Tkinter as tk
+else:
+    import tkinter as tk
