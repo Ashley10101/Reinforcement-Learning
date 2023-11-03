e74196c001f250176d14d3a98df15305ce2717bf
Ashely <Yiyun.L.Evelyn@gmail.com>
2023-11-03 23:26:01 +0800

    submit

---
+++ Sarsa/Maze/RL_brain.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+class RL(object):
+    def __init__(self, action_space, learning_rate = 0.01, reward_decay = 0.9, e_greedy = 0.1):
+        self.actions = action_space
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon = e_greedy
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            self.q_table = self.q_table.append(
+                pd.Series(
+                    [0]*len(self.actions),
+                    index = self.q_table.columns,
+                    name = state,
+                )
+            )
+    
+    def choose_action(self, observation):
+        self.check_state_exist(observation)
+        if np.random.rand() < self.epsilon:
+            action = np.random.choice(self.actions)
+        else:
+            state_action = self.q_table.loc[observation, :]
+            action = np.random.choice(state_action[state_action==max(state_action)].index)
+        return action
+    
+    def learn(self, *args):
+        pass
+
+class SarsaTable(RL):
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        super(SarsaTable, self).__init__(actions, learning_rate, reward_decay, e_greedy)
+
+    def learn(self, s, a, r, s_, a_):
+        self.check_state_exist(s_)
+        q_predict = self.q_table.loc[s, a]
+        if s != 'terminal':
+            q_target = r + self.gamma*self.q_table.loc[s_, a_]
+        else:
+            q_target = r
+        self.q_table.loc[s,a] += self.lr*(q_target - q_predict)
