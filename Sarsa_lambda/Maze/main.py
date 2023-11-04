7ba9d4c4ec8791509faea762494a39386c4ba88c
Ashely <Yiyun.L.Evelyn@gmail.com>
2023-11-04 21:06:41 +0800

    submit

---
+++ Sarsa_lambda/Maze/main.py
@@ -0,0 +1,29 @@
+from env import Maze
+from RL_brain import SarsaLambdaTable
+
+def update():
+    for episode in range(100):
+        observation = env.reset()
+        action = RL.choose_action(str(observation))
+        RL.eligibility_trace *= 0
+        while True:
+            env.render()
+            observation_, reward, done = env.step(action)
+            action_  = RL.choose_action(str(observation_))
+            RL.learn(str(observation), action, reward, str(observation_), action_)
+            action = action_
+            observation = observation_
+            if done:
+                break
+
+    # end of game
+    print('game over')
+    env.destroy()
+
+
+if __name__ == '__main__':
+    env = Maze()
+    RL = SarsaLambdaTable(actions=list(range(env.n_actions)))
+
+    env.after(100, update)
+    env.mainloop()
